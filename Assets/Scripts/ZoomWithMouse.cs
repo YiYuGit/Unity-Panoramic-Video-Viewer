2853038b08b1cc0c19f7e24cfd80bbb98abdae20
YiYuGit <68971742+YiYuGit@users.noreply.github.com>
2021-08-30 14:03:08 -0400

    Upload

---
+++ Assets/Scripts/ZoomWithMouse.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Zoom forward and backward with mousewheel, Attach this script to camera
+public class ZoomWithMouse : MonoBehaviour
+{
+    public float zoomSpeed = 20f;
+    public float fovMin = 3f, fovMax = 150f;
+    private float zoom = 10;
+    private float camFov = 90f;
+
+
+    private void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+        zoom = zoom - (zoomSpeed * Input.GetAxis("Mouse ScrollWheel"));
+        camFov = Mathf.Clamp(zoom, fovMin, fovMax);
+        Debug.Log(camFov);
+
+        Camera.main.fieldOfView = camFov;
+    }
+}
---
+++ readme.txt
@@ -0,0 +1,6 @@
+This unity project can play pano videos.
+The video is attached to a video sphere with video player. 
+The camera is placed at the center of the sphere, and can rotate/zoom to see video.
+The sphere's material neeed to use "Flip normals" shader, so the video is played from the inside of the video.
+
+Since the shader is flipped, the video source also needed to be flipped, so the viwer can see the correct video.
